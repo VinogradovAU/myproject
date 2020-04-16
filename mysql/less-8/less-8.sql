d21f2fc552365f7c4fe48c881b8147998c042d17
Vinogradov Andrey <ezh24@yandex.ru>
2020-04-16 14:35:42 +0300

    update task 3 for tasks less 8 mysql

--- mysql/less-8/less-8.sql
+++ mysql/less-8/less-8.sql
@@ -291,12 +291,33 @@ mysql> SELECT CONCAT(u.first_name," ",u.last_name) AS user,
 mysql>
 ВЫВОД ТАБЛИЦЫ АКТИВНОСТИ ВСЕХ ПОЛЬЗОВАТЕЛЕЙ:
 
+
+ДОбавил COUNT в сумму, чтобы проссумировать несколько значений для конкретного пользователя.
+
+SELECT CONCAT(u.first_name," ",u.last_name) AS user,
+	p.user_id AS posts,
+    m.from_user_id AS messages,
+    media.user_id AS media,
+    l.user_id AS likes,
+    (COUNT(IFNULL(p.user_id,0)) + COUNT(IFNULL(m.from_user_id,0)) + COUNT(IFNULL(media.user_id,0)) + COUNT(IFNULL(l.user_id,0))) AS `user activity`
+
+    FROM users AS u
+    LEFT JOIN posts AS p
+    ON p.user_id = u.id
+    LEFT JOIN messages AS m
+    ON m.from_user_id = u.id
+    LEFT JOIN media AS media
+    ON media.user_id = u.id
+    LEFT JOIN likes AS l
+    ON l.user_id = u.id
+    GROUP BY u.id
+    ORDER BY `user activity` ASC
 mysql> SELECT CONCAT(u.first_name," ",u.last_name) AS user,
     -> p.user_id AS posts,
     ->     m.from_user_id AS messages,
     ->     media.user_id AS media,
     ->     l.user_id AS likes,
-    ->     (IFNULL(p.user_id,0) + IFNULL(m.from_user_id,0) + IFNULL(media.user_id,0) + IFNULL(l.user_id,0)) AS `user activity`
+    ->     (COUNT(IFNULL(p.user_id,0)) + COUNT(IFNULL(m.from_user_id,0)) + COUNT(IFNULL(media.user_id,0)) + COUNT(IFNULL(l.user_id,0))) AS `user activity`
     ->
     ->     FROM users AS u
     ->     LEFT JOIN posts AS p
@@ -308,115 +329,148 @@ mysql> SELECT CONCAT(u.first_name," ",u.last_name) AS user,
     ->     LEFT JOIN likes AS l
     ->     ON l.user_id = u.id
     ->     GROUP BY u.id
-    ->     ORDER BY `user activity` ASC;
+    ->     ORDER BY `user activity` ASC
+    ->     limit 100;
 +----------------------+-------+----------+-------+-------+---------------+
 | user                 | posts | messages | media | likes | user activity |
 +----------------------+-------+----------+-------+-------+---------------+
-| Simone Beahan        |  NULL |     NULL |     1 |     1 |             2 |
-| Annamarie Johns      |     3 |     NULL |  NULL |  NULL |             3 |
-| Toney Halvorson      |     2 |     NULL |  NULL |     2 |             4 |
+| Wiley Ullrich        |    58 |     NULL |  NULL |    58 |             4 |
+| Imogene Breitenberg  |  NULL |       78 |  NULL |    78 |             4 |
+| Aglae Emard          |  NULL |       71 |    71 |    71 |             4 |
+| Dahlia Anderson      |  NULL |     NULL |    26 |    26 |             4 |
+| Laisha Stanton       |    66 |     NULL |    66 |  NULL |             4 |
+| Maci Murphy          |  NULL |     NULL |    64 |    64 |             4 |
+| Elmo Marquardt       |    30 |     NULL |    30 |  NULL |             4 |
+| Catalina Bogisich    |    34 |     NULL |    34 |    34 |             4 |
+| Paris Lebsack        |    35 |       35 |    35 |  NULL |             4 |
+| Vladimir Dibbert     |    24 |     NULL |    24 |  NULL |             4 |
+| Kiley Rohan          |    37 |     NULL |    37 |    37 |             4 |
+| Preston Bayer        |  NULL |       54 |  NULL |    54 |             4 |
+| Brycen Howe          |  NULL |     NULL |    51 |  NULL |             4 |
+| Alda Donnelly        |    49 |       49 |    49 |    49 |             4 |
+| Rosa Hoeger          |    48 |     NULL |    48 |    48 |             4 |
+| Baby Hamill          |  NULL |     NULL |    44 |    44 |             4 |
+| Jody Reilly          |  NULL |       45 |  NULL |    45 |             4 |
+| Ryan Braun           |    46 |       46 |  NULL |  NULL |             4 |
+| Bette Boehm          |  NULL |     NULL |    93 |    93 |             4 |
+| Drew Runolfsdottir   |    11 |     NULL |    11 |    11 |             4 |
+| Maryjane Koss        |  NULL |        9 |  NULL |     9 |             4 |
+| Sheldon Leannon      |    79 |     NULL |  NULL |  NULL |             4 |
+| Rodrick Ortiz        |  NULL |       87 |    87 |    87 |             4 |
+| Matilda Watsica      |  NULL |     NULL |    83 |  NULL |             4 |
+| Zoie Hayes           |  NULL |     NULL |    16 |  NULL |             4 |
+| Marion Legros        |  NULL |       17 |  NULL |    17 |             4 |
+| Annamarie Johns      |     3 |     NULL |  NULL |  NULL |             4 |
+| Chet Hills           |    99 |     NULL |  NULL |    99 |             4 |
+| Lilliana Powlowski   |    95 |     NULL |    95 |  NULL |             8 |
+| Levi Boehm           |    60 |       60 |  NULL |  NULL |             8 |
+| Maximillian Keeling  |    53 |     NULL |    53 |  NULL |             8 |
+| Mariana Thompson     |    96 |       96 |    96 |    96 |             8 |
+| Anya Russel          |    92 |       92 |    92 |  NULL |             8 |
+| Amara Stiedemann     |    56 |       56 |    56 |    56 |             8 |
+| Waylon Lakin         |  NULL |       98 |  NULL |    98 |             8 |
+| Roberto Kulas        |    91 |       91 |  NULL |    91 |             8 |
+| Raquel Morissette    |  NULL |     NULL |    90 |  NULL |             8 |
+| Kirstin Gislason     |  NULL |       61 |  NULL |  NULL |             8 |
+| William Turcotte     |    63 |       63 |  NULL |    63 |             8 |
+| Jonathan Ritchie     |  NULL |       65 |    65 |    65 |             8 |
+| Serena Cormier       |    82 |     NULL |    82 |  NULL |             8 |
+| Claudie Larson       |    70 |     NULL |  NULL |  NULL |             8 |
+| Toney Halvorson      |     2 |     NULL |  NULL |     2 |             8 |
+| Crawford Homenick    |    72 |       72 |    72 |    72 |             8 |
+| Chance Littel        |    73 |     NULL |    73 |    73 |             8 |
+| Maximilian Bosco     |    76 |     NULL |    76 |    76 |             8 |
+| Adrian Fay           |    22 |       22 |    22 |    22 |             8 |
+| Jean Ryan            |    23 |       23 |    23 |  NULL |             8 |
+| Simone Beahan        |  NULL |     NULL |     1 |     1 |             8 |
 | Katlyn Ritchie       |     4 |        4 |  NULL |  NULL |             8 |
-| Cordie Raynor        |    10 |     NULL |  NULL |  NULL |            10 |
-| Beaulah Bernier      |     5 |        5 |  NULL |     5 |            15 |
-| Ebony Pollich        |     8 |     NULL |     8 |  NULL |            16 |
-| Zoie Hayes           |  NULL |     NULL |    16 |  NULL |            16 |
-| Maryjane Koss        |  NULL |        9 |  NULL |     9 |            18 |
-| Julien Beier         |     7 |        7 |  NULL |     7 |            21 |
+| Cordie Raynor        |    10 |     NULL |  NULL |  NULL |             8 |
+| Aubrey Klein         |  NULL |     NULL |  NULL |    39 |             8 |
+| Ebony Pollich        |     8 |     NULL |     8 |  NULL |             8 |
+| Julien Beier         |     7 |        7 |  NULL |     7 |             8 |
+| Alexzander Gottlieb  |  NULL |     NULL |    31 |  NULL |             8 |
+| Devan McClure        |    33 |     NULL |  NULL |    33 |            12 |
+| Emelie Ferry         |  NULL |       25 |    25 |  NULL |            12 |
+| Jonathon Price       |  NULL |     NULL |    89 |    89 |            12 |
+| Beau Spencer         |    13 |     NULL |    13 |  NULL |            12 |
+| Meghan Hyatt         |    19 |       19 |  NULL |  NULL |            12 |
+| Cody O'Hara          |    59 |       59 |  NULL |    59 |            12 |
+| Mariano Spencer      |    36 |       36 |  NULL |    36 |            12 |
+| Dedrick O'Reilly     |    38 |     NULL |  NULL |  NULL |            12 |
+| Deanna Reinger       |    86 |     NULL |  NULL |  NULL |            12 |
+| Maximus Koepp        |  NULL |       42 |  NULL |    42 |            12 |
+| Kristy Reichel       |  NULL |       14 |    14 |    14 |            12 |
+| Alize Eichmann       |  NULL |       94 |    94 |    94 |            16 |
+| Rose Pollich         |  NULL |       18 |    18 |  NULL |            16 |
+| Gabrielle Swift      |  NULL |       12 |    12 |    12 |            16 |
+| Enola Monahan        |    47 |       47 |    47 |    47 |            16 |
+| Lucio Bauch          |  NULL |       81 |    81 |  NULL |            16 |
+| Kristian Deckow      |    84 |     NULL |  NULL |    84 |            16 |
+| Rasheed Pagac        |  NULL |       69 |    69 |    69 |            16 |
+| Annamae Rosenbaum    |    27 |       27 |    27 |  NULL |            16 |
+| Sydnee Wunsch        |    29 |       29 |    29 |    29 |            16 |
+| Johnathan Kihn       |  NULL |       62 |  NULL |    62 |            16 |
+| Holly Bailey         |    57 |     NULL |  NULL |  NULL |            16 |
+| Beaulah Bernier      |     5 |        5 |  NULL |     5 |            16 |
+| Jerel Christiansen   |    97 |       97 |  NULL |    97 |            16 |
+| Michele Schimmel     |    50 |       50 |    50 |    50 |            16 |
+| Justice Jast         |   100 |      100 |  NULL |   100 |            16 |
+| Teagan Botsford      |  NULL |     NULL |    74 |    74 |            24 |
 | Dustin Little        |     6 |        6 |     6 |     6 |            24 |
-| Beau Spencer         |    13 |     NULL |    13 |  NULL |            26 |
-| Unique Willms        |  NULL |     NULL |    15 |    15 |            30 |
-| Alexzander Gottlieb  |  NULL |     NULL |    31 |  NULL |            31 |
-| Drew Runolfsdottir   |    11 |     NULL |    11 |    11 |            33 |
-| Marion Legros        |  NULL |       17 |  NULL |    17 |            34 |
-| Gabrielle Swift      |  NULL |       12 |    12 |    12 |            36 |
-| Rose Pollich         |  NULL |       18 |    18 |  NULL |            36 |
-| Meghan Hyatt         |    19 |       19 |  NULL |  NULL |            38 |
-| Dedrick O'Reilly     |    38 |     NULL |  NULL |  NULL |            38 |
-| Aubrey Klein         |  NULL |     NULL |  NULL |    39 |            39 |
-| Kristy Reichel       |  NULL |       14 |    14 |    14 |            42 |
-| Vladimir Dibbert     |    24 |     NULL |    24 |  NULL |            48 |
-| Emelie Ferry         |  NULL |       25 |    25 |  NULL |            50 |
-| Brycen Howe          |  NULL |     NULL |    51 |  NULL |            51 |
-| Dahlia Anderson      |  NULL |     NULL |    26 |    26 |            52 |
-| Holly Bailey         |    57 |     NULL |  NULL |  NULL |            57 |
-| Samara Carter        |    20 |     NULL |    20 |    20 |            60 |
-| Elmo Marquardt       |    30 |     NULL |    30 |  NULL |            60 |
-| Kirstin Gislason     |  NULL |       61 |  NULL |  NULL |            61 |
-| Devan McClure        |    33 |     NULL |  NULL |    33 |            66 |
-| Jean Ryan            |    23 |       23 |    23 |  NULL |            69 |
-| Claudie Larson       |    70 |     NULL |  NULL |  NULL |            70 |
-| Sheldon Leannon      |    79 |     NULL |  NULL |  NULL |            79 |
-| Annamae Rosenbaum    |    27 |       27 |    27 |  NULL |            81 |
-| Sister Pacocha       |  NULL |       41 |  NULL |    41 |            82 |
-| Matilda Watsica      |  NULL |     NULL |    83 |  NULL |            83 |
-| Howard Harvey        |  NULL |       28 |    28 |    28 |            84 |
-| Carissa Heller       |    21 |       21 |    21 |    21 |            84 |
-| Maximus Koepp        |  NULL |       42 |  NULL |    42 |            84 |
-| Deanna Reinger       |    86 |     NULL |  NULL |  NULL |            86 |
-| Adrian Fay           |    22 |       22 |    22 |    22 |            88 |
-| Baby Hamill          |  NULL |     NULL |    44 |    44 |            88 |
-| Raquel Morissette    |  NULL |     NULL |    90 |  NULL |            90 |
-| Jody Reilly          |  NULL |       45 |  NULL |    45 |            90 |
-| Ryan Braun           |    46 |       46 |  NULL |  NULL |            92 |
-| Catalina Bogisich    |    34 |     NULL |    34 |    34 |           102 |
-| Eliseo Hagenes       |    52 |     NULL |    52 |  NULL |           104 |
-| Paris Lebsack        |    35 |       35 |    35 |  NULL |           105 |
-| Maximillian Keeling  |    53 |     NULL |    53 |  NULL |           106 |
-| Mariano Spencer      |    36 |       36 |  NULL |    36 |           108 |
-| Preston Bayer        |  NULL |       54 |  NULL |    54 |           108 |
-| Kiley Rohan          |    37 |     NULL |    37 |    37 |           111 |
-| Sydnee Wunsch        |    29 |       29 |    29 |    29 |           116 |
-| Wiley Ullrich        |    58 |     NULL |  NULL |    58 |           116 |
-| Levi Boehm           |    60 |       60 |  NULL |  NULL |           120 |
-| Jonathon Stoltenberg |  NULL |       40 |    40 |    40 |           120 |
-| Johnathan Kihn       |  NULL |       62 |  NULL |    62 |           124 |
-| Aurore Abernathy     |    32 |       32 |    32 |    32 |           128 |
-| Maci Murphy          |  NULL |     NULL |    64 |    64 |           128 |
-| Catalina Wilkinson   |  NULL |       43 |    43 |    43 |           129 |
-| Laisha Stanton       |    66 |     NULL |    66 |  NULL |           132 |
-| Rosa Hoeger          |    48 |     NULL |    48 |    48 |           144 |
-| Teagan Botsford      |  NULL |     NULL |    74 |    74 |           148 |
-| Imogene Breitenberg  |  NULL |       78 |  NULL |    78 |           156 |
-| Lucio Bauch          |  NULL |       81 |    81 |  NULL |           162 |
-| Serena Cormier       |    82 |     NULL |    82 |  NULL |           164 |
-| Kristian Deckow      |    84 |     NULL |  NULL |    84 |           168 |
-| Cade D'Amore         |    85 |     NULL |    85 |  NULL |           170 |
-| Cody O'Hara          |    59 |       59 |  NULL |    59 |           177 |
-| Jonathon Price       |  NULL |     NULL |    89 |    89 |           178 |
-| Bette Boehm          |  NULL |     NULL |    93 |    93 |           186 |
-| Enola Monahan        |    47 |       47 |    47 |    47 |           188 |
-| William Turcotte     |    63 |       63 |  NULL |    63 |           189 |
-| Lilliana Powlowski   |    95 |     NULL |    95 |  NULL |           190 |
-| Jonathan Ritchie     |  NULL |       65 |    65 |    65 |           195 |
-| Alda Donnelly        |    49 |       49 |    49 |    49 |           196 |
-| Waylon Lakin         |  NULL |       98 |  NULL |    98 |           196 |
-| Chet Hills           |    99 |     NULL |  NULL |    99 |           198 |
-| Michele Schimmel     |    50 |       50 |    50 |    50 |           200 |
-| Narciso Heller       |    67 |       67 |  NULL |    67 |           201 |
-| Rasheed Pagac        |  NULL |       69 |    69 |    69 |           207 |
-| Aglae Emard          |  NULL |       71 |    71 |    71 |           213 |
-| Chance Littel        |    73 |     NULL |    73 |    73 |           219 |
-| Aron Gorczany        |    55 |       55 |    55 |    55 |           220 |
-| Amara Stiedemann     |    56 |       56 |    56 |    56 |           224 |
-| Constantin Ryan      |    75 |     NULL |    75 |    75 |           225 |
-| Maximilian Bosco     |    76 |     NULL |    76 |    76 |           228 |
-| Danielle Kerluke     |  NULL |       77 |    77 |    77 |           231 |
-| Rodrick Ortiz        |  NULL |       87 |    87 |    87 |           261 |
-| Hassan Lind          |  NULL |       88 |    88 |    88 |           264 |
-| Trinity Zulauf       |    68 |       68 |    68 |    68 |           272 |
-| Roberto Kulas        |    91 |       91 |  NULL |    91 |           273 |
-| Anya Russel          |    92 |       92 |    92 |  NULL |           276 |
-| Alize Eichmann       |  NULL |       94 |    94 |    94 |           282 |
-| Crawford Homenick    |    72 |       72 |    72 |    72 |           288 |
-| Jerel Christiansen   |    97 |       97 |  NULL |    97 |           291 |
-| Justice Jast         |   100 |      100 |  NULL |   100 |           300 |
-| Eve Hand             |    80 |       80 |    80 |    80 |           320 |
-| Mariana Thompson     |    96 |       96 |    96 |    96 |           384 |
+| Catalina Wilkinson   |  NULL |       43 |    43 |    43 |            24 |
+| Danielle Kerluke     |  NULL |       77 |    77 |    77 |            24 |
+| Jonathon Stoltenberg |  NULL |       40 |    40 |    40 |            24 |
+| Howard Harvey        |  NULL |       28 |    28 |    28 |            24 |
+| Constantin Ryan      |    75 |     NULL |    75 |    75 |            24 |
+| Cade D'Amore         |    85 |     NULL |    85 |  NULL |            32 |
+| Sister Pacocha       |  NULL |       41 |  NULL |    41 |            32 |
+| Narciso Heller       |    67 |       67 |  NULL |    67 |            32 |
+| Eve Hand             |    80 |       80 |    80 |    80 |            32 |
+| Eliseo Hagenes       |    52 |     NULL |    52 |  NULL |            36 |
+| Unique Willms        |  NULL |     NULL |    15 |    15 |            40 |
+| Samara Carter        |    20 |     NULL |    20 |    20 |            48 |
+| Aurore Abernathy     |    32 |       32 |    32 |    32 |            48 |
+| Aron Gorczany        |    55 |       55 |    55 |    55 |            48 |
+| Hassan Lind          |  NULL |       88 |    88 |    88 |            48 |
+| Carissa Heller       |    21 |       21 |    21 |    21 |            64 |
+| Trinity Zulauf       |    68 |       68 |    68 |    68 |           240 |
 +----------------------+-------+----------+-------+-------+---------------+
 100 rows in set (0,00 sec)
 
 mysql>
+mysql> SELECT CONCAT(u.first_name," ",u.last_name) AS user,
+    -> p.user_id AS posts,
+    ->     m.from_user_id AS messages,
+    ->     media.user_id AS media,
+    ->     l.user_id AS likes,
+    ->     (COUNT(IFNULL(p.user_id,0)) + COUNT(IFNULL(m.from_user_id,0)) + COUNT(IFNULL(media.user_id,0)) + COUNT(IFNULL(l.user_id,0))) AS `user activity`
+    ->
+    ->     FROM users AS u
+    ->     LEFT JOIN posts AS p
+    ->     ON p.user_id = u.id
+    ->     LEFT JOIN messages AS m
+    ->     ON m.from_user_id = u.id
+    ->     LEFT JOIN media AS media
+    ->     ON media.user_id = u.id
+    ->     LEFT JOIN likes AS l
+    ->     ON l.user_id = u.id
+    ->     GROUP BY u.id
+    ->     ORDER BY `user activity` ASC
+    ->     limit 10;
++--------------------+-------+----------+-------+-------+---------------+
+| user               | posts | messages | media | likes | user activity |
++--------------------+-------+----------+-------+-------+---------------+
+| Elmo Marquardt     |    30 |     NULL |    30 |  NULL |             4 |
+| Paris Lebsack      |    35 |       35 |    35 |  NULL |             4 |
+| Annamarie Johns    |     3 |     NULL |  NULL |  NULL |             4 |
+| Dahlia Anderson    |  NULL |     NULL |    26 |    26 |             4 |
+| Zoie Hayes         |  NULL |     NULL |    16 |  NULL |             4 |
+| Marion Legros      |  NULL |       17 |  NULL |    17 |             4 |
+| Catalina Bogisich  |    34 |     NULL |    34 |    34 |             4 |
+| Maryjane Koss      |  NULL |        9 |  NULL |     9 |             4 |
+| Vladimir Dibbert   |    24 |     NULL |    24 |  NULL |             4 |
+| Drew Runolfsdottir |    11 |     NULL |    11 |    11 |             4 |
++--------------------+-------+----------+-------+-------+---------------+
+10 rows in set (0,00 sec)
 
-Не понимаю, где нарушена логика. Моя идея такая: построчно для каждого пользователя сложить
-количество постов, сообщений, лайков и единиц медиаданных. Полученная сумма - это значение активности.
-Чем меньше это число, тем пользователь менее активен.
+mysql>
