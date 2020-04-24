b4626886098eae495fd9f1dbb6dfef08bdd4be2a
Vinogradov Andrey <ezh24@yandex.ru>
2020-04-24 12:52:12 +0300

    tasks mysql-lesson-10-02

--- mysql/less-10/less-10.sql
+++ mysql/less-10/less-10.sql
@@ -9,13 +9,7 @@
 #отношение в процентах (количество пользователей в группе / всего пользователей в системе) * 100
 
 --------------------mysql workbench----------------------
-SELECT
-	xx1.UU2 AS name_group,                         # имя группы
-    xx1.pioples_in_group,                          # количество пользователей в группе
-    #xx1.xxxx.date_youngest_user,                  # день рождения самого молодого
-    xx1.younger_user,                              # самый молодой пользователь группы
-    #yy1.yyyy.date_oldest_user,                    # день рождения самого старшего
-    yy1.oldest_user                                # самы взрослый пользователь группы
+
 SELECT
 	xx1.UU2 AS name_group,                         # имя группы
     xx1.pioples_in_group,                          # количество пользователей в группе
@@ -24,19 +18,31 @@ SELECT
     #yy1.yyyy.date_oldest_user,                    # день рождения самого старшего
     yy1.oldest_user                                # самы взрослый пользователь группы
     yy1.count_users AS count_users                 # всего пользователей в системе
+    ROUND(yy1.count_users/zz1.zz0) AS avg_in_groups    # среднее количество пользователей в группАХ
+
 FROM
+  (SELECT count(*) AS zz0
+	FROM
+		(select count(*)
+        FROM communities AS c                 <---------тут я считал количество групп для расчета средне количества пользователей в группах
+        JOIN communities_users AS cc
+        ON c.id = cc.community_id
+        group by c.name) AS ccc
+  ) AS zz1,
+
 (SELECT xxxx.us AS UU2, xxxx.pioples_in_group AS pioples_in_group  , xxxx.date_youngest_user, users.last_name AS younger_user
 	FROM
     (SELECT DISTINCT communities.name AS us,
  COUNT(*) OVER(PARTITION BY communities_users.community_id) AS pioples_in_group,
  MIN(profiles.birthday) OVER(PARTITION BY communities_users.community_id) AS date_oldest_user,
  MAX(profiles.birthday) OVER(PARTITION BY communities_users.community_id) AS date_youngest_user
+
  FROM users AS users
     JOIN profiles
     ON users.id = profiles.user_id
-    LEFT JOIN communities_users
+    JOIN communities_users
     ON communities_users.user_id = users.id
-    LEFT JOIN communities
+    JOIN communities
     ON communities.id = communities_users.community_id) xxxx, users, profiles
     WHERE
     users.id = profiles.user_id AND profiles.birthday = date_youngest_user) AS xx1,
@@ -51,9 +57,9 @@ FROM
  FROM users AS users
     JOIN profiles
     ON users.id = profiles.user_id
-    LEFT JOIN communities_users
+    JOIN communities_users
     ON communities_users.user_id = users.id
-    LEFT JOIN communities
+    JOIN communities
     ON communities.id = communities_users.community_id) yyyy, users, profiles
     WHERE
     users.id = profiles.user_id AND profiles.birthday = date_oldest_user) AS yy1
@@ -69,20 +75,33 @@ mysql> SELECT
     ->     xx1.younger_user,
     ->     #yy1.yyyy.date_oldest_user,
     ->     yy1.oldest_user,
-    ->     yy1.count_users AS count_users
+    ->     yy1.count_users AS count_users,
+    ->     ROUND(yy1.count_users/zz1.zz0) AS avg_in_groups
     -> FROM
+    ->   (SELECT count(*) AS zz0
+    -> FROM
+    ->
+Display all 891 possibilities? (y or n)
+    -> (select count(*)
+    ->         FROM communities AS c
+    ->         JOIN communities_users AS cc
+    ->         ON c.id = cc.community_id
+    ->         group by c.name) AS ccc
+    ->   ) AS zz1,
+    ->
     -> (SELECT xxxx.us AS UU2, xxxx.pioples_in_group AS pioples_in_group  , xxxx.date_youngest_user, users.last_name AS younger_user
     -> FROM
     ->     (SELECT DISTINCT communities.name AS us,
     ->  COUNT(*) OVER(PARTITION BY communities_users.community_id) AS pioples_in_group,
     ->  MIN(profiles.birthday) OVER(PARTITION BY communities_users.community_id) AS date_oldest_user,
     ->  MAX(profiles.birthday) OVER(PARTITION BY communities_users.community_id) AS date_youngest_user
+    ->
     ->  FROM users AS users
     ->     JOIN profiles
     ->     ON users.id = profiles.user_id
-    ->     LEFT JOIN communities_users
+    ->     JOIN communities_users
     ->     ON communities_users.user_id = users.id
-    ->     LEFT JOIN communities
+    ->     JOIN communities
     ->     ON communities.id = communities_users.community_id) xxxx, users, profiles
     ->     WHERE
     ->     users.id = profiles.user_id AND profiles.birthday = date_youngest_user) AS xx1,
@@ -97,25 +116,25 @@ mysql> SELECT
     ->  FROM users AS users
     ->     JOIN profiles
     ->     ON users.id = profiles.user_id
-    ->     LEFT JOIN communities_users
+    ->     JOIN communities_users
     ->     ON communities_users.user_id = users.id
-    ->     LEFT JOIN communities
+    ->     JOIN communities
     ->     ON communities.id = communities_users.community_id) yyyy, users, profiles
     ->     WHERE
     ->     users.id = profiles.user_id AND profiles.birthday = date_oldest_user) AS yy1
     ->     WHERE yy1.UU1 = xx1.UU2;
-+------------+------------------+--------------+-------------+-------------+
-| name_group | pioples_in_group | younger_user | oldest_user | count_users |
-+------------+------------------+--------------+-------------+-------------+
-| droup4     |               13 | Larson       | Halvorson   |         100 |
-| droup2     |               14 | McClure      | Reilly      |         100 |
-| droup7     |               10 | Lebsack      | Hoeger      |         100 |
-| droup6     |               13 | Bauch        | Hagenes     |         100 |
-| droup3     |                5 | Christiansen | Bayer       |         100 |
-| droup8     |               15 | Wunsch       | Ullrich     |         100 |
-| droup1     |               16 | Zulauf       | Lind        |         100 |
-| droup5     |               14 | Anderson     | Hills       |         100 |
-+------------+------------------+--------------+-------------+-------------+
-8 rows in set (0,01 sec)
++------------+------------------+--------------+-------------+-------------+---------------+
+| name_group | pioples_in_group | younger_user | oldest_user | count_users | avg_in_groups |
++------------+------------------+--------------+-------------+-------------+---------------+
+| droup4     |               13 | Larson       | Halvorson   |         100 |            13 |
+| droup2     |               14 | McClure      | Reilly      |         100 |            13 |
+| droup7     |               10 | Lebsack      | Hoeger      |         100 |            13 |
+| droup6     |               13 | Bauch        | Hagenes     |         100 |            13 |
+| droup3     |                5 | Christiansen | Bayer       |         100 |            13 |
+| droup8     |               15 | Wunsch       | Ullrich     |         100 |            13 |
+| droup1     |               16 | Zulauf       | Lind        |         100 |            13 |
+| droup5     |               14 | Anderson     | Hills       |         100 |            13 |
++------------+------------------+--------------+-------------+-------------+---------------+
+8 rows in set (0,00 sec)
 
 mysql>
