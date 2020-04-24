d89ab99aa17135c636e2c27d4e0cedf6afe31a47
Vinogradov Andrey <ezh24@yandex.ru>
2020-04-24 13:00:08 +0300

    tasks mysql-lesson-10-03

--- mysql/less-10/less-10.sql
+++ mysql/less-10/less-10.sql
@@ -16,15 +16,15 @@ SELECT
     #xx1.xxxx.date_youngest_user,                  # день рождения самого молодого
     xx1.younger_user,                              # самый молодой пользователь группы
     #yy1.yyyy.date_oldest_user,                    # день рождения самого старшего
-    yy1.oldest_user                                # самы взрослый пользователь группы
-    yy1.count_users AS count_users                 # всего пользователей в системе
-    ROUND(yy1.count_users/zz1.zz0) AS avg_in_groups    # среднее количество пользователей в группАХ
-
+    yy1.oldest_user,                               # самы взрослый пользователь группы
+    yy1.count_users AS count_users ,                                       # всего пользователей в системе
+    ROUND(yy1.count_users/zz1.zz0) AS avg_in_groups,                       # среднее количество пользователей в группАХ
+    ROUND(xx1.pioples_in_group/yy1.count_users * 100, 2) AS '%%groups_user/all_user'           # %
 FROM
   (SELECT count(*) AS zz0
 	FROM
 		(select count(*)
-        FROM communities AS c                 <---------тут я считал количество групп для расчета средне количества пользователей в группах
+        FROM communities AS c
         JOIN communities_users AS cc
         ON c.id = cc.community_id
         group by c.name) AS ccc
@@ -69,14 +69,15 @@ FROM
 Вывод в терминале:
 
 mysql> SELECT
-    -> xx1.UU2 AS name_group,
-    ->     xx1.pioples_in_group,
-    ->     #xx1.xxxx.date_youngest_user,
-    ->     xx1.younger_user,
-    ->     #yy1.yyyy.date_oldest_user,
-    ->     yy1.oldest_user,
-    ->     yy1.count_users AS count_users,
-    ->     ROUND(yy1.count_users/zz1.zz0) AS avg_in_groups
+    -> xx1.UU2 AS name_group,                         # имя группы
+    ->     xx1.pioples_in_group,                          # количество пользователей в группе
+    ->     #xx1.xxxx.date_youngest_user,                  # день рождения самого молодого
+    ->     xx1.younger_user,                              # самый молодой пользователь группы
+    ->     #yy1.yyyy.date_oldest_user,                    # день рождения самого старшего
+    ->     yy1.oldest_user,                               # самы взрослый пользователь группы
+    ->     yy1.count_users AS count_users ,                                       # всего пользователей в системе
+    ->     ROUND(yy1.count_users/zz1.zz0) AS avg_in_groups,                       # среднее количество пользователей в группАХ
+    ->     ROUND(xx1.pioples_in_group/yy1.count_users * 100, 2) AS '%%groups_user/all_user'           # %
     -> FROM
     ->   (SELECT count(*) AS zz0
     -> FROM
@@ -123,18 +124,18 @@ Display all 891 possibilities? (y or n)
     ->     WHERE
     ->     users.id = profiles.user_id AND profiles.birthday = date_oldest_user) AS yy1
     ->     WHERE yy1.UU1 = xx1.UU2;
-+------------+------------------+--------------+-------------+-------------+---------------+
-| name_group | pioples_in_group | younger_user | oldest_user | count_users | avg_in_groups |
-+------------+------------------+--------------+-------------+-------------+---------------+
-| droup4     |               13 | Larson       | Halvorson   |         100 |            13 |
-| droup2     |               14 | McClure      | Reilly      |         100 |            13 |
-| droup7     |               10 | Lebsack      | Hoeger      |         100 |            13 |
-| droup6     |               13 | Bauch        | Hagenes     |         100 |            13 |
-| droup3     |                5 | Christiansen | Bayer       |         100 |            13 |
-| droup8     |               15 | Wunsch       | Ullrich     |         100 |            13 |
-| droup1     |               16 | Zulauf       | Lind        |         100 |            13 |
-| droup5     |               14 | Anderson     | Hills       |         100 |            13 |
-+------------+------------------+--------------+-------------+-------------+---------------+
++------------+------------------+--------------+-------------+-------------+---------------+------------------------+
+| name_group | pioples_in_group | younger_user | oldest_user | count_users | avg_in_groups | %%groups_user/all_user |
++------------+------------------+--------------+-------------+-------------+---------------+------------------------+
+| droup4     |               13 | Larson       | Halvorson   |         100 |            13 |                  13.00 |
+| droup2     |               14 | McClure      | Reilly      |         100 |            13 |                  14.00 |
+| droup7     |               10 | Lebsack      | Hoeger      |         100 |            13 |                  10.00 |
+| droup6     |               13 | Bauch        | Hagenes     |         100 |            13 |                  13.00 |
+| droup3     |                5 | Christiansen | Bayer       |         100 |            13 |                   5.00 |
+| droup8     |               15 | Wunsch       | Ullrich     |         100 |            13 |                  15.00 |
+| droup1     |               16 | Zulauf       | Lind        |         100 |            13 |                  16.00 |
+| droup5     |               14 | Anderson     | Hills       |         100 |            13 |                  14.00 |
++------------+------------------+--------------+-------------+-------------+---------------+------------------------+
 8 rows in set (0,00 sec)
 
 mysql>
