d6537c2d827ae8e90c5bc7ae5b551a792e9b7cc4
Vinogradov Andrey <ezh24@yandex.ru>
2020-08-23 21:27:56 +0300

    tasks-4 ok + brodilka

---
+++ js_basic_course/less_4/8 бродилка (в дз)/mover.js
@@ -0,0 +1,91 @@
+let mover = {
+    /**
+     * Получает и отдает направление от пользователя.
+     * @returns {int} Возвращаем направление, введенное пользователем.
+     */
+    getDirection() {
+        // Доступные значения ввода.
+        const availableDirections = [2, 4, 6, 8, 1, 7, 9, 3];
+
+        while (true) {
+            // Получаем от пользователя направление.
+            let direction = parseInt(prompt('Введите число (2, 4, 6, 8 или 1, 7, 9, 3), куда вы хотите переместиться, "Отмена" для выхода.'));
+            if (isNaN(direction)) {
+                return null;
+            }
+            // Если направление не одно из доступных, то сообщаем что надо ввести корректные данные
+            // и начинаем новую итерацию.
+            if (!availableDirections.includes(direction)) {
+                alert('Для перемещения необходимо ввести одно из чисел 2, 4, 6 или 8.');
+                continue;
+            }
+
+            // Если пользователь ввел корректное значение - отдаем его.
+            return direction;
+        }
+    },
+
+    /**
+     * Отдает следующую точку в которой будет находиться пользователь после движения.
+     * @param {int} direction Направление движения игрока.
+     * @returns {{x: int, y: int}} Следующая позиция игрока.
+     */
+    getNextPosition(direction) {
+        // Следующая точка игрока, в самом начале в точке будут текущие координаты игрока.
+        const nextPosition = {
+            x: player.x,
+            y: player.y,
+        };
+        // Определяем направление и обновляем местоположение игрока в зависимости от направления.
+        switch (direction) {
+            case 2:
+                if (nextPosition.y < 9) {
+                    nextPosition.y++;
+                };
+                break;
+            case 4:
+                if (nextPosition.x > 0) {
+                    nextPosition.x--;
+                }
+                break;
+            case 6:
+                if (nextPosition.x < 9) {
+                    nextPosition.x++;
+                }
+                break;
+            case 8:
+                if (nextPosition.y > 0) {
+                    nextPosition.y--;
+                }
+                break;
+
+                //добавление новых клавишь
+            case 1:
+                if (nextPosition.x > 0 && nextPosition.y < 9) {
+                    nextPosition.x--;
+                    nextPosition.y++;
+                }
+                break;
+            case 7:
+                if (nextPosition.x > 0 && nextPosition.y > 0) {
+                    nextPosition.x--;
+                    nextPosition.y--;
+                }
+                break;
+            case 9:
+                if (nextPosition.y > 0 && nextPosition.x < 9) {
+                    nextPosition.y--;
+                    nextPosition.x++;
+                }
+                break;
+            case 3:
+                if (nextPosition.x < 9 && nextPosition.y < 9) {
+                    nextPosition.x++;
+                    nextPosition.y++;
+                }
+                break;
+        }
+
+        return nextPosition;
+    },
+};
